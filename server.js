9783eaaeab3683131bfbfe2826d7df6138ede986
mmiller1234321 <148365315+mmiller1234321@users.noreply.github.com>
2024-05-02 08:19:12 -0500

    Can't forget the A's, added team history

--- server.js
+++ server.js
@@ -122,6 +122,18 @@ app.get('/search', (req, res) => {
         AND (t.name = ? OR t.name = ?)
     `;
     params = [playerName, position, team, 'Milwaukee Braves'];
+  } else if (team === 'Oakland Athletics' || team === 'Kansas City Athletics' || team === 'Philadelphia Athletics') {
+    query = `
+      SELECT MAX(b.${stat}) AS max_stat_value
+      FROM batting AS b
+      JOIN people AS p ON b.playerID = p.playerID
+      JOIN fielding AS f ON b.playerID = f.playerID
+      JOIN teams AS t ON b.teamID = t.teamID
+      WHERE CONCAT(p.nameFirst, ' ', p.nameLast) = ? 
+        AND f.POS = ?
+        AND (t.name = ? OR t.name = ? OR t.name = ?)
+    `;
+    params = [playerName, position, team, 'Kansas City Athletics', 'Philadelphia Athletics'];
   } else {
     // Default logic for other teams
     query = `
