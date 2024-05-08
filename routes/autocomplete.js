e4b4b15750ea09877bcd3208fb7204366f8f87ae
mmiller1234321 <148365315+mmiller1234321@users.noreply.github.com>
2024-05-08 10:05:53 -0500

    axios added to package.json

--- routes/autocomplete.js
+++ routes/autocomplete.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../db'); // Assuming you have a database connection pool initialized
+const pool = require('../db/db.js'); // Assuming you have a database connection pool initialized
 
 router.get('/', (req, res) => {
   const query = req.query.query;
