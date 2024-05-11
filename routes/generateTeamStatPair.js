555e6f73e8c646e13126ffbd479e25156f4ed354
mmiller1234321 <148365315+mmiller1234321@users.noreply.github.com>
2024-05-10 22:52:46 -0500

    server.js typo

--- routes/generateTeamStatPair.js
+++ routes/generateTeamStatPair.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../db/db.js');
 
-// Function to fetch the next row from the database
-function fetchNextRow() {
+// Function to generate the next team and stat pair
+function generateNextTeamStatPair() {
   // Fetch the first row from the generated_tables table
   pool.query('SELECT team_name, stat_name FROM generated_tables LIMIT 1', (error, results) => {
     if (error) {
@@ -12,7 +12,7 @@ function fetchNextRow() {
       if (results.length > 0) {
         const teamName = results[0].team_name;
         const statName = results[0].stat_name;
-        
+
         // Insert the fetched data into the gameboard table without the date
         pool.query('INSERT INTO gameboard (team_name, stat_name) VALUES (?, ?)', [teamName, statName], (error, results) => {
           if (error) {
@@ -22,7 +22,7 @@ function fetchNextRow() {
         });
 
         // Start timer for 72 hours
-        setTimeout(fetchNextRow, 72 * 60 * 60 * 1000);
+        setTimeout(generateNextTeamStatPair, 72 * 60 * 60 * 1000);
       } else {
         console.log('No team and stat pairs found in the database');
       }
@@ -30,30 +30,11 @@ function fetchNextRow() {
   });
 }
 
-// Fetch the first row on server start
-fetchNextRow();
+// Generate the first team and stat pair on server start
+generateNextTeamStatPair();
 
-// Route to handle fetching the team and stat pair
-router.get('/', (req, res, next) => {
-  pool.query('SELECT team_name, stat_name FROM gameboard ORDER BY date ASC LIMIT 1', (error, results) => {
-    if (error) {
-      console.error('Error fetching team and stat pair:', error);
-      res.status(500).send('Internal Server Error');
-    } else {
-      if (results.length > 0) {
-        const teamName = results[0].team_name;
-        const statName = results[0].stat_name;
-        res.json({ team: teamName, stat: statName });
-      } else {
-        console.log('No team and stat pairs found in the gameboard table');
-        res.status(404).send('No team and stat pairs found');
-      }
-    }
-  });
-});
-
-// Route to handle fetching the gameboard
-router.get('/fetch-gameboard', (req, res, next) => {
+// Route to handle fetching the next team and stat pair
+router.get('/team-stat-pair', (req, res, next) => {
   pool.query('SELECT team_name, stat_name FROM gameboard ORDER BY date ASC LIMIT 1', (error, results) => {
     if (error) {
       console.error('Error fetching team and stat pair:', error);
