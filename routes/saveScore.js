774d710d047b38554a90ca56ae1c142d9921afee
mmiller1234321 <148365315+mmiller1234321@users.noreply.github.com>
2024-05-12 22:36:06 -0500

    changed route name in index.html, saveScore

--- routes/saveScore.js
+++ routes/saveScore.js
@@ -2,20 +2,18 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../db/db.js');
 
-// POST route to save total score along with team_name and stat_name into games table
 router.post('/', (req, res) => {
   const { totalScore, teamName, statName, gameboardId } = req.body;
 
-  // Insert the score into the games table along with the fetched gameboardId
   pool.query(
     'INSERT INTO games (team_name, stat_name, total_score, gameboard_id) VALUES (?, ?, ?, ?)',
     [teamName, statName, totalScore, gameboardId],
     (error, results, fields) => {
       if (error) {
         console.error('Error inserting total score:', error);
-        res.status(500).json({ error: 'An error occurred while inserting total score' });
+        res.status 500).json({ error: 'An error occurred while inserting total score' });
       } else {
-        console.log('Total score inserted successfully:', totalScore); // Log the total score
+        console.log('Total score inserted successfully:', totalScore);
         res.status(200).json({ message: 'Total score inserted successfully' });
       }
     }
@@ -23,6 +21,7 @@ router.post('/', (req, res) => {
 });
 
 module.exports = router;
+
 
 
 
