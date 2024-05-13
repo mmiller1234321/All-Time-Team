78b80890430e50169b3ec531f8c107fc277a4b39
mmiller1234321 <148365315+mmiller1234321@users.noreply.github.com>
2024-05-13 06:40:33 -0500

    updated logs to check saveScore

--- routes/saveScore.js
+++ routes/saveScore.js
@@ -4,13 +4,13 @@ const pool = require('../db/db.js');
 
 router.post('/', (req, res) => {
   const { totalScore, teamName, statName, gameboardId } = req.body;
-
   console.log('Attempting to insert score:', totalScore, 'for team:', teamName, 'and stat:', statName, 'with gameboard ID:', gameboardId);
 
+  // Ensure your SQL query and column names are accurate and the connection is properly established.
   pool.query(
     'INSERT INTO games (team_name, stat_name, total_score, gameboard_id) VALUES (?, ?, ?, ?)',
     [teamName, statName, totalScore, gameboardId],
-    (error, results, fields) => {
+    (error, results) => {
       if (error) {
         console.error('Error inserting total score:', error);
         res.status(500).json({ error: 'An error occurred while inserting total score' });
@@ -23,6 +23,7 @@ router.post('/', (req, res) => {
 });
 
 module.exports = router;
+
 
 
 
