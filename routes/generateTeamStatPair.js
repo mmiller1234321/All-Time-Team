7845e0e31cf1e54377ea12f0e201475bcbd3bf28
mmiller1234321 <148365315+mmiller1234321@users.noreply.github.com>
2024-05-16 05:44:23 -0500

    reset setTimeout timer to 24 hours

--- routes/generateTeamStatPair.js
+++ routes/generateTeamStatPair.js
@@ -10,7 +10,7 @@ function generateNextTeamStatPair() {
   pool.getConnection((err, connection) => {
     if (err) {
       console.error('Error getting MySQL connection:', err);
-      setTimeout(generateNextTeamStatPair, 8 * 60 * 1000);
+      setTimeout(generateNextTeamStatPair, 24 * 60 * 60 * 1000); // Set timeout to 24 hours
       return;
     }
 
@@ -18,7 +18,7 @@ function generateNextTeamStatPair() {
       if (error) {
         console.error('Error executing MySQL query:', error);
         connection.release();
-        setTimeout(generateNextTeamStatPair, 8 * 60 * 1000);
+        setTimeout(generateNextTeamStatPair, 24 * 60 * 60 * 1000); // Set timeout to 24 hours
         return;
       }
 
@@ -36,14 +36,14 @@ function generateNextTeamStatPair() {
             } else {
               console.log(`Inserted ${teamName} - ${statName} - ${perfectScore} into gameboard`);
             }
-            setTimeout(generateNextTeamStatPair, 8 * 60 * 1000);
+            setTimeout(generateNextTeamStatPair, 24 * 60 * 60 * 1000); // Set timeout to 24 hours
           }
         );
       } else {
         connection.release();
         console.log('Reached end of table, restarting cycle.');
         lastFetchedId = 0;
-        setTimeout(generateNextTeamStatPair, 8 * 60 * 1000);
+        setTimeout(generateNextTeamStatPair, 24 * 60 * 60 * 1000); // Set timeout to 24 hours
       }
     });
   });
@@ -75,6 +75,7 @@ router.get('/team-stat-pair', (req, res) => {
 });
 
 module.exports = router;
+
 
 
 
